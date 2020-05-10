b6ba46d9bbba16bd7722b40a0af01046a9d3cb19
Fernando A de la Fuente <FernandoAdelafuente@gmail.com>
2020-05-10 15:40:16 -0700

    Update Fluids_project.m

--- Fluids_project.m
+++ Fluids_project.m
@@ -17,11 +17,11 @@ clc
 
 g= 9.81;
 rho = 800; %775-840 kg/m^2
-nozzle_d = .01 %%.3; % m
-v_init = 17; %m/s
+nozzle_d = .2; %%.3; % m
+v_init = 10; %m/s
 mdot = rho * v_init * (nozzle_d^2*pi / 4);
 
-a_y = @(v) g - .2*nozzle_d^2 * v^2; %with gravity, y direction
+a_y = @(v) g - .2*nozzle_d^2 * v^2 * 1.225/g; %with gravity, y direction
 a_x = @(v) .2*nozzle_d^2 * v^2;
 
 %y stuff
@@ -29,31 +29,29 @@ altitude = 5;
 velocity_y = v_init;
 
 %x stuff
-x = 0;
+x_dir = 0;
 velocity_x = v_init;
 
 step = .01;
+target_alt = 15;
+delta = 1;
 
-v_at_10 = 1;
-while v_at_10>=0
+while abs(delta)>.001
     n=1;
-    while altitude(n) <= 15 && sign(velocity_y(n)) == 1
+    velocity_y(n) = v_init;
+    while sign(velocity_y(n)) == 1
         velocity_y(n+1) = velocity_y(n) - step * a_y(velocity_y(n));
         altitude(n+1) = altitude(n) + sind(60)*velocity_y(n+1) * step;
         velocity_x(n+1) = velocity_x(n) - step * a_x(velocity_x(n));
-        x(n+1) = x(n) + velocity_x(n)*step;
+        x_dir(n+1) = x_dir(n) + velocity_x(n)*step;
         n = n+1;
     end
-    v_at_10 = velocity_y(length(velocity_y))
-    if v_at_10>=0
-        clear velocity_y altitude velocity_x x 
-        v_init = v_init-.0001;
-        altitude = 5;
-        velocity_y = v_init;
-        %x stuff
-        x = 0;
-        velocity_x = v_init;
-    end
+    delta = target_alt - altitude(length(altitude));
+    y = altitude; %save in case we like it
+    x = x_dir; %same as above
+    x_dir = 0;
+    altitude = 5;
+    v_init = v_init + delta/10;
 end
-
-plot(x,altitude,'.k')
+v_init
+plot(x,y,'.k')
